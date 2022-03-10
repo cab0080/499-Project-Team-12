44946813dbd94b4a5692462a47dea14a371f65f9
Cole <zandebergen.cole@gmail.com>
2022-03-10 16:59:49 -0600

    added password verification

--- public_html/PHPScripts/login.php
+++ public_html/PHPScripts/login.php
@@ -2,21 +2,28 @@
     include 'connect.php'; //include the script that opens the database connection
     $connection = OpenCon();
     $form_username = $_POST['username']; //this is the username that is entered by the site visitor
+    $form_password = $_POST['password']; //This is the password that the user entered
      
     //Now, let's run an SQL query and save those results in a variable
     $result = $connection->query("SELECT * FROM `Agent` WHERE `username` LIKE '$form_username'");
 
     
-    //echo how many results actually showed up here
-    $numberOfResults = $result->num_rows;
-    if($numberOfResults == 0){
-        echo "We did NOT get results";
-        echo "<br />The username we searched for was " . $form_username;
-        echo "<br />The query that we ran was " . "SELECT * FROM `Agent` WHERE `username` LIKE '$form_username'";
+    //Find out if there is exactly one result, otherwise we have a problem
+    if($result->num_rows == 0){
+        echo "This username was not found.";
     }
-    else if($numberOfResults == 1){
-        echo "We got this many results: " . $numberOfResults;
-        echo "<br />This person's email address is " . $result->fetch_assoc()["email"];
+    else if($result->num_rows == 1){
+        $passhash = $result->fetch_assoc()["passHash"];
+        echo "username found in database!<br />Checking password...";
+        if(password_verify($form_password, $passhash)){
+            echo "You entered the correct password! - welcome";
+        }
+        else{
+            echo "You entered the wrong password!";
+        }
+    }
+    else{
+        echo "Error: There are more than one users with this username";
     }
     /*echo "The name is: ";
     echo $_POST['username'];*/
