8cf8bacc620446d3347f4e158fda3a279ba357f6
Cole <zandebergen.cole@gmail.com>
2022-04-11 11:02:21 -0500

    added functionality to delete a showing from the showing list

--- public_html/PHPScripts/deleteShowing.php
+++ public_html/PHPScripts/deleteShowing.php
@@ -1,5 +1,13 @@
 <?php
+    include 'connect.php';
+    include 'errorMessage.php';
+    $connection = OpenCon();
     $number = $_GET['MLS'];
     $time = $_GET['time'];
-    echo "delete showing where mls=" . $number . " and time=" . $time;
+    $sql = "DELETE from Showing WHERE MLSNumber='$number' AND startDatetime='$time'";
+    $result = $connection->query($sql);
+    if($result != 1){
+        setMessage("There was an error deleting the showing <br> " . $connection->error);
+    }
+    header("Location: ../showing_schedule.php");
 ?>
