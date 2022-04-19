bf98a2af1b94031d38c8810c2523e2fafe3167d6
Corey <corey.bouldin0711@gmail.com>
2022-04-19 00:17:45 -0500

    Updated frontend of feedback pages so they look cleaner.

--- public_html/submit_feedback.php
+++ public_html/submit_feedback.php
@@ -2,8 +2,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
     <link rel="stylesheet" type="text/css" href="stylesheets/styleguide.css" />
     <link rel="stylesheet" type="text/css" href="stylesheets/globals.css" />
+    <link rel="stylesheet" type="text/css" href="stylesheets/feedback-page.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <?php
         $_SESSION['number'] = $_GET['number'];
@@ -12,16 +15,18 @@
 </head>
 
 <body style="margin: 0; background: #f5f5f5">
-    <header><div class="banner"></div>
+    <header>
+        <div class="banner"></div>
         <div class="tucasanacom">tucasana.com</div>
-        <div class="addlisting">Submit Feedback</div></header>
+        <div class="feedback">Submit Feedback</div>
+    </header>
         <form style="margin-top: 250px;padding: 20px;" enctype="multipart/form-data" method="post" action="PHPScripts/submitFeedback.php">
             <div class="row">
-                <div class="col">
-                    <label>Rate the customer's interest in the property:</label>
+                <div class="col-xxl-4">
+                    <label class="form-label">Rate the customer's interest in the property:</label>
                 </div>
                 <div class="col">
-                    <select class="combo" name="customerInterest" type="text" list="value">
+                    <select class="form-select" name="customerInterest" type="text" list="value" style="width: 300px;">
                         <option value="Interested">Interested</option>
                         <option value="Somewhat Interested">Somewhat interested</option>
                         <option value="Neutral">Neutral</option>
@@ -31,11 +36,11 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col">
-                    <label>Please rate your overall experience at this showing:</label>
+                <div class="col-xxl-4">
+                    <label class="form-label">Please rate your overall experience at this showing:</label>
                 </div>
                 <div class="col">
-                    <select name="overallExperience" type="text">
+                    <select class="form-select" name="overallExperience" type="text" style="width: 100px;">
                         <option value="1">1</option>
                         <option value="2">2</option>
                         <option value="3">3</option>
@@ -50,11 +55,11 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col">
-                    <label>What is your and the customer's opinion of the price?</label>
+                <div class="col-xxl-4">
+                    <label class="form-label">What is your and the customer's opinion of the price?</label>
                 </div>
                 <div class="col">
-                    <select class="combo" name="priceOpinion" type="text">
+                    <select class="form-select" name="priceOpinion" type="text" style="width: 300px;">
                         <option value="Way too high">Way too high</option>
                         <option value="A little high">A little high</option>
                         <option value="Fair">Fair</option>
@@ -64,11 +69,11 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col">
-                    <label class="form-label">Please enter any general additional feedback here</label>
+                <div class="col-xxl-4">
+                    <label class="form-label">Please enter any general additional feedback here:</label>
                 </div>
                 <div class="col">
-                    <textarea name="generalFeedback" placeholder="General Feedback here..."></textarea>
+                    <textarea name="generalFeedback" class="form-control" style="height: 100px;width: 300px;" placeholder="General Feedback here..."></textarea>
                 </div>
             </div>
             <button class="btn btn-primary" type="submit">Submit</button>
